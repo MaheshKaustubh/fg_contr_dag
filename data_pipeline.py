d494149b5700dbb93d4a067caf50008debf8c1ef
MaheshKaustubh <143107934+MaheshKaustubh@users.noreply.github.com>
2024-01-05 19:28:15 +0530

    Update data_pipeline.py

--- data_pipeline.py
+++ data_pipeline.py
@@ -36,7 +36,7 @@ with DAG(
     'fieldglass_dag',
     default_args=default_args,
     description='DAG to load file to Snowflake from SFTP',
-    schedule_interval='57 13 * * *',
+    schedule_interval='0 14 * * *',
     catchup=False,
 ) as dag:
     WDcheck = ShortCircuitOperator(
