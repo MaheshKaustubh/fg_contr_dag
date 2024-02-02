dd8bc367510be118a4e78b44a13c9b41842220cc
MaheshKaustubh <143107934+MaheshKaustubh@users.noreply.github.com>
2024-02-02 15:03:49 +0530

    Update data_pipeline.py

--- data_pipeline.py
+++ data_pipeline.py
@@ -24,7 +24,7 @@ default_args = {
 
 def check():
     from datetime import datetime
-    wds=[datetime(2024,1,22).date(),datetime(2024,2,1).date(), datetime(2024, 3, 1).date(), datetime(2024, 4, 1).date(),
+    wds=[datetime(2024,1,22).date(),datetime(2024,2,2).date(), datetime(2024, 3, 1).date(), datetime(2024, 4, 1).date(),
         datetime(2024, 5, 2).date(), datetime(2024, 6, 3).date(), datetime(2024, 7, 1).date(),
         datetime(2024, 8, 1).date(), datetime(2024, 9, 2).date(), datetime(2024, 10, 1).date(),
         datetime(2024, 4, 11).date(), datetime(2024, 12, 2).date()]
@@ -196,7 +196,7 @@ with DAG(
     'fieldglass_dag',
     default_args=default_args,
     description='DAG to load file to Snowflake from SFTP',
-    schedule_interval='30 9 * * *',
+    schedule_interval='38 9 * * *',
     catchup=False,
 ) as dag:
     WDcheck = ShortCircuitOperator(
